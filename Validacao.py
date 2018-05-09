14e5b7ecf2708fd01392b05a095cd5bf68b037bd
danielzf2 <38058371+danielzf2@users.noreply.github.com>
2018-05-09 14:27:48 -0300

    Add files via upload

---
+++ Validacao.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+def equacaodiferencial_tempo(Tf,t,Potencia,Hs,As,To,Dp,Kp,Ap,Hp,M,C):
+    x= Potencia
+    y= -Hs * As *(Tf-To)
+    z= -((Tf-To)/((Dp/(Kp*Ap))+(1/(Hp*Ap))))
+    w= M * C
+    dTdt= (x+y+z)/w
+
+    return dTdt
+#criando a lista tempo
+
+lista_tempo= np.arange(0,3,10**-2)
+#definindo os valores
+Potencia=270
+Hs=60
+As=0.003
+To=20+273
+Dp=0.001
+Kp=0.17
+Ap=0.005
+Hp=0.03
+M=0.720
+C=41.86
+
+#fazendo a solução usando o Odeint
+solucao=odeint(equacaodiferencial_tempo,To,lista_tempo, args=(Potencia,Hs,As,To,Dp,Kp,Ap,Hp,M,C,))
+
+
+plt.plot(lista_tempo,solucao)
+plt.xlabel("Tempo(minutos)")
+plt.ylabel("Temperatura(kelvin)")
+plt.grid(True)
+plt.show()
+
